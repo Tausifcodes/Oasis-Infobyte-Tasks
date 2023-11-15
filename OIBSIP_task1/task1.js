03e66be9fe8ab86d74f55f609507fdd4a0c38cd1
Tausifcodes <119741137+Tausifcodes@users.noreply.github.com>
2023-11-15 19:53:39 +0530

    Add files via upload

---
+++ OIBSIP_task1/task1.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+$('.slider').slick({
+  arrows: false,
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+})
+});
+
